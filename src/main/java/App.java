cfd14d992e1acdb115b2da2de4f6544adb7ec912
Premnath E <premnathprem125@gmail.com>
2026-04-16 17:10:24 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,6 @@ public static int add(int a, int b) {
 return a + b; 
 } 
 public static void main(String[] args) { 
-System.out.println("Hello DevOps!"); 
+System.out.println("Hello DevOps!");
 } 
 }
